1b5840b3db72838b24ccec4fa87642df10ba022d
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-26 16:05:23 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-
+// declarar global {
 declare global {
+  // prisma: Cliente Prisma ou indefinido;
   var prisma: PrismaClient | undefined;
 }
-
+// prisma ligando a = prisma.global Ou novo ClientePrisma();
 const prisma = global.prisma || new PrismaClient();
-
+// Se (process.env.NODE_ENV === "desenvolvedor") prisma.global ligando a = prisma; for verdade
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
-
+// exportar por padrão prisma;
 export default prisma;
