55e2dea532dc51fb0fd4533e6b460b06e81f9b2a
leonardopontes <126637474+leonardopontes@users.noreply.github.com>
2023-10-26 15:26:32 -0300

    Update auth-status.tsx

--- components/auth-status.tsx
+++ components/auth-status.tsx
@@ -1,6 +1,7 @@
 import { unstable_getServerSession } from "next-auth/next";
-
+// exportar por padrão função assíncrona de Status de Autenticação {
 export default async function AuthStatus() {
+  // sessão ligando a = aguardar pegar Sessão de Servidor_instável();
   const session = await unstable_getServerSession();
   return (
     <div className="absolute top-5 w-full flex justify-center items-center">
